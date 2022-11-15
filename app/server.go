361e7c13e3ed48871e25d6a49e3e0305e9225c48
Anoop Elias <anoopelias@gmail.com>
2022-11-15 19:06:13 +0530

    Stage 1

--- app/server.go
+++ app/server.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	// Uncomment this block to pass the first stage
-	// "net"
-	// "os"
+	"net"
+	"os"
 )
 
 func main() {
@@ -12,15 +12,14 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage
-	//
-	// l, err := net.Listen("tcp", "0.0.0.0:6379")
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to port 6379")
-	// 	os.Exit(1)
-	// }
-	// _, err = l.Accept()
-	// if err != nil {
-	// 	fmt.Println("Error accepting connection: ", err.Error())
-	// 	os.Exit(1)
-	// }
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
+	_, err = l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
 }
