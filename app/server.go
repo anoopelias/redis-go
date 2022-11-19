021f9fad0e3570f43e5a33c3673b1387ae149de8
Anoop Elias <anoopelias@gmail.com>
2022-11-19 17:21:55 +0530

    Cleanup comments and logs

--- app/server.go
+++ app/server.go
@@ -3,17 +3,11 @@ package main
 import (
 	"bufio"
 	"fmt"
-
-	// Uncomment this block to pass the first stage
 	"net"
 	"os"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
