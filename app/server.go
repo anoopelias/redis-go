7c8adf73b73beccf0ec78c9a761b8a27874d3d18
Anoop Elias <anoopelias@gmail.com>
2022-11-20 21:20:36 +0530

    Stage 4

--- app/server.go
+++ app/server.go
@@ -14,11 +14,17 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go execute(conn)
 	}
+}
+
+func execute(conn net.Conn) {
 	r := bufio.NewReader(conn)
 
 	for {
